0578a27a9a17e5f92ba229434bf2ed8db77e044a
grinnsi <anton.may@gmx.net>
2025-04-23 11:20:12 +0200

    Add configuration file for GeoBasis Loader plugin settings and variables

---
+++ config.py
@@ -0,0 +1,15 @@
+import os
+from qgis.core import QgsSettings
+
+PLUGIN_VERSION = '1.2'
+PLUGIN_NAME = 'GeoBasis Loader'
+PLUGIN_NAME_AND_VERSION = PLUGIN_NAME + ' (v' + PLUGIN_VERSION + ')'
+MY_CRITICAL_1 = 'Layerladefehler '
+MY_CRITICAL_2 = ', Dienst nicht verfügbar (URL?)'
+MY_INFO_1 = 'Layer '
+MY_INFO_2 = ' erfolgreich geladen.'
+
+PLUGIN_DIR = os.path.dirname(__file__)
+USER_SETTINGS = QgsSettings()
+CURRENT_CATALOG_SETTINGS_KEY = 'geobasis_loader/current_catalog'
+AUTOMATIC_CRS_SETTINGS_KEY = 'geobasis_loader/automatic_crs'
