f88a1ccf8b264b801a08aafc6e6485cf0899cfe8
grinnsi <anton.may@gmx.net>
2025-08-06 23:20:03 +0200

    Add SERVER_SETTINGS_KEY and implement server retrieval method for only enabled servers

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 from enum import Enum
 import os
+from qgis.core import QgsSettings
 
 PLUGIN_VERSION = '1.4'
 PLUGIN_NAME = 'GeoBasis Loader'
@@ -12,6 +13,7 @@ MY_INFO_2 = ' erfolgreich geladen.'
 PLUGIN_DIR = os.path.dirname(__file__)
 CURRENT_CATALOG_SETTINGS_KEY = 'geobasis_loader/current_catalog'
 AUTOMATIC_CRS_SETTINGS_KEY = 'geobasis_loader/automatic_crs'
+SERVERS_SETTINGS_KEY = 'geobasis_loader/servers'
 
 CATALOG_OVERVIEW = "GeoBasis_Loader_v5_Kataloge.json"
 CATALOG_OVERVIEW_NAME = "catalog_overview"
@@ -20,8 +22,20 @@ class ServerHosts(str, Enum):
     GITHUB = "https://api.github.com/repos/geoObserver/geobasis_loader/contents/kataloge/{name}?ref=main"
     
     @classmethod
-    def get_servers(cls) -> list[str]:
+    def get_all_servers(cls) -> list[str]:
         return [a.value for a in cls]
+    
+    @classmethod
+    def get_enabled_servers(cls) -> list[str]:
+        servers = []
+        qgs_settings = QgsSettings()
+        server_index = qgs_settings.value(SERVERS_SETTINGS_KEY, 0)
+        if server_index == 0:
+            servers = cls.get_all_servers()
+        else:
+            servers.append(cls.get_all_servers()[server_index - 1])
+            
+        return servers
     
 class InternalProperties(str, Enum):
     VISIBILITY = "__visible__"
