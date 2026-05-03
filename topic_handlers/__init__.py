eee6cd090461cd9685a69229c90f5059acb18b69
grinnsi <anton.may@gmx.net>
2026-05-03 11:39:06 +0200

    fix(init): added PresetManager singleton and PresetDialog to init
    Added PresetManager singleton and PresetDialog to init, so these are better accessible and in line with other classes.

--- topic_handlers/__init__.py
+++ topic_handlers/__init__.py
@@ -1,2 +1,3 @@
 from .property_manager import singleton as PropertyManager
 from .catalog_manager import singleton as CatalogManager
+from .preset_manager import singleton as PresetManager
--- ui/__init__.py
+++ ui/__init__.py
@@ -1,3 +1,4 @@
 from .settings_dialog import SettingsDialog
 from .epsg_dialog import EpsgDialog
+from .preset_dialog import PresetDialog
 from . import icons as Icons                # Uppercase to be inline with other imports?
